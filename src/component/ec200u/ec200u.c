2f66c5ec4edb36a070a6b565da6c425a48232c7a
KhanhDQ <qkhanhh2998@gmail.com>
2023-02-20 22:50:12 +0700

    fix: add CRLF in AT expect message

--- src/component/ec200u/ec200u.c
+++ src/component/ec200u/ec200u.c
@@ -6,7 +6,7 @@ bool Ec200uCheckATCmd(Ec200u* this) {
     buff = StringAppendString(buff, EC200U_CMD_CHECK_AT);
     AT_MODEM_BUILD_TAIL(buff);
 
-    return ATmodemExcutiveCmd((ATmodem*)this, cmd, "OK", EC200U_NORMAL_CMD_TIMEOUT_MS, 3);
+    return ATmodemExcutiveCmd((ATmodem*)this, cmd, "OK\r\n", EC200U_NORMAL_CMD_TIMEOUT_MS, 3);
 }
 
 bool Ec200uTurnOffEcho(Ec200u* this) {
@@ -15,7 +15,7 @@ bool Ec200uTurnOffEcho(Ec200u* this) {
     buff = StringAppendString(buff, EC200U_CMD_TURN_OFF_ECHO);
     AT_MODEM_BUILD_TAIL(buff);
 
-    return ATmodemExcutiveCmd((ATmodem*)this, cmd, "OK", EC200U_NORMAL_CMD_TIMEOUT_MS, 3);
+    return ATmodemExcutiveCmd((ATmodem*)this, cmd, "OK\r\n", EC200U_NORMAL_CMD_TIMEOUT_MS, 3);
 }
 
 bool Ec200uCheckSimReady(Ec200u* this) {
@@ -26,7 +26,7 @@ bool Ec200uCheckSimReady(Ec200u* this) {
     buff = StringAppendString(buff, EC200U_CMD_CHECK_NETWORK_STT);
     AT_MODEM_BUILD_TAIL(buff);
 
-    return ATmodemExcutiveCmd((ATmodem*)this, cmd, "+CREG: 0,2", EC200U_NORMAL_CMD_TIMEOUT_MS, 10);
+    return ATmodemExcutiveCmd((ATmodem*)this, cmd, "+CREG: 0,2\r\n", EC200U_NORMAL_CMD_TIMEOUT_MS, 10);
 }
 
 bool Ec200uOpenNetwork(Ec200u* this, char* host, char* port) {
@@ -54,7 +54,7 @@ bool Ec200uCloseNetwork(Ec200u* this) {
     buff = StringAppendString(buff, "0");
     AT_MODEM_BUILD_TAIL(buff);
 
-    return ATmodemExcutiveCmd((ATmodem*)this, cmd, "+QMTCLOSE: 0,0", 30000, 3);
+    return ATmodemExcutiveCmd((ATmodem*)this, cmd, "+QMTCLOSE: 0,0\r\n", 30000, 3);
 }
 
 bool Ec200uConnectServer(Ec200u* this, char* clientID, char* username, char* password) {
@@ -72,7 +72,7 @@ bool Ec200uConnectServer(Ec200u* this, char* clientID, char* username, char* pas
     buff = StringAppendStringWithQuote(buff, password);
     AT_MODEM_BUILD_TAIL(buff);
 
-    return ATmodemExcutiveCmd((ATmodem*)this, cmd, "+QMTCONN: 0,0,0", 5000, 3);
+    return ATmodemExcutiveCmd((ATmodem*)this, cmd, "+QMTCONN: 0,0,0\r\n", 5000, 3);
 }
 
 bool Ec200uDisconnectServer(Ec200u* this) {
@@ -84,7 +84,7 @@ bool Ec200uDisconnectServer(Ec200u* this) {
     buff = StringAppendString(buff, "0");
     AT_MODEM_BUILD_TAIL(buff);
 
-    return ATmodemExcutiveCmd((ATmodem*)this, cmd, "+QMTDISC: 0,0", 30000, 3);
+    return ATmodemExcutiveCmd((ATmodem*)this, cmd, "+QMTDISC: 0,0\r\n", 30000, 3);
 }
 
 bool Ec200uPublishMessage(Ec200u* this, char* qos, char* retain, char* topic, char* message) {
@@ -110,6 +110,6 @@ bool Ec200uPublishMessage(Ec200u* this, char* qos, char* retain, char* topic, ch
     if (!isReadyPublish)
         return false;
 
-    return ATmodemExcutiveCmd((ATmodem*)this, message, "+QMTPUBEX: 0,0,0", 10000, 1);
+    return ATmodemExcutiveCmd((ATmodem*)this, message, "+QMTPUBEX: 0,0,0\r\n", 10000, 1);
 }
 
